eea75ca1eb3bae23d6808ac917d515afa0c95ee2
K41 <136763358+K41-max@users.noreply.github.com>
2024-04-01 19:21:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def get_info(request):
     global version
     return json.dumps([version,os.environ.get('RENDER_EXTERNAL_URL'),str(request.headers),str(request.url)])
 
-@app.get("/", response_class=HTMLResponse)
+@app.route("/")
 def home():
     return redirect("/bbs")
 
-@app.get("/bbs", response_class=HTMLResponse)
+@app.route("/bbs")
 def view_bbs():
     return render_template("bbs.html")
 
-@app.get("/bbs/info", response_class=HTMLResponse)
+@app.route("/bbs/info")
 def view_bbs_info():
     return requests.get(f"{url}bbs/info").text
 
-@app.get("/bbs/api", response_class=HTMLResponse)
+@app.route("/bbs/api")
 def view_bbs_api():
     t = requests.get(f"{url}bbs/api?t={urllib.parse.quote(str(int(time.time()*1000)))}&verify=false&channel=main", cookies={"yuki":"True"}, allow_redirects=False)
     if t.status_code != 307:
         return Response(t.text)
     return redirect("/bbs")
 
-@app.get("/bbs/result")
+@app.route("/bbs/result")
 def write_bbs():
     name = request.args.get("name", "")
     message = request.args.get("message", "")
@@ -49,16 +49,16 @@ def write_bbs():
         return Response(t.text)
     return redirect(f"/bbs?name={urllib.parse.quote(name)}&seed={urllib.parse.quote(seed)}&channel={urllib.parse.quote(channel)}&verify={urllib.parse.quote(verify)}")
 
-@app.get("/bbs/how", response_class=PlainTextResponse)
+@app.route("/bbs/how")
 def view_commonds():
     return requests.get(f"{url}bbs/how").text
 
-@app.get("/load_instance")
+@app.route("/load_instance")
 def load_instance():
     global url
     url = requests.get('https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 
-@app.get("/bbs/howtouse", response_class=HTMLResponse)
+@app.route("/bbs/howtouse")
 def view_bbs_how_to_use():
     return render_template("bbshow.html")
 
